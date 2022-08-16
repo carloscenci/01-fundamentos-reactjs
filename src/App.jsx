8bb7c69a516bfa2fa4e54d299f8dbc24523213ba
carloscenci <carlosc.estevam@gmail.com>
2022-08-16 17:05:12 -0300

    sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
-function App() {
+import Header from './components/Header';
+import Post from './Post';
+
+import './global.css'
+import styles from './App.module.css'
+import { SideBar } from './components/Sidebar';
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post 
+            author="Carlos Cenci" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quisquam vitae at et sequi officia harum! Quidem repellat, eos dolores, architecto enim in ab consectetur harum alias, illum tempore atque!" 
+          />
+          <Post 
+            author="Alexa" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quisquam vitae at et sequi officia harum! Quidem repellat, eos dolores, architecto enim in ab consectetur harum alias, illum tempore atque!" 
+          />
+        </main>
+      </div>
+    </div>
   )
 }
 
